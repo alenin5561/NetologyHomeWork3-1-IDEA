60ac27c1950429fbd72ad2380a6076d5dc0fc28f
Аленин Андрей <aleninmailbox@gmail.com>
2022-06-21 15:02:52 +0300

    Расчет бонуса

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Объявляем переменные для входных данных и
+        // параметров программы:
+        int ticket = 1450;//одну для хранения стоимости билета
+        int rub = 20;//другую для хранения количества рублей для одной бонусной милли
+
+        System.out.println("Ваш бонус от стоимости билета, в милях:");
+        System.out.println(ticket / rub);
+        System.out.println("Мили");
+
+    }
+}
